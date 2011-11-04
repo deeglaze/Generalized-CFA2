236a0abd9df16853ab6de226b40435f68678d165
James Ian Johnson <ianj@moonpatrol.ccs.neu.edu>
2011-11-04 13:38:39 -0400

    Tests passing for generalized CFA2.

--- clinger.rkt
+++ clinger.rkt
@@ -7,14 +7,16 @@
 (provide (all-defined-out))
 
 (define coloring (make-hasheq))
-(define (escapes? x) (dict-ref coloring x #t))
+(define names (make-hasheq))
+(define (in-stack? x) (dict-ref coloring x #f))
 
 ;; An implementation of Istack from [Clinger 98]
 ;; - set! + call/cc + display with stack and heap separated.
 
 (define-language Scheme
   [e (e e ...) ref lam
-     (if e e e) (call/cc e) (qwote c)
+     (if e e e) (qwote c)
+     #;(call/cc e)
      #;(set! ref e)]
   [ref x]
   [lam (λ (x ...) e)]
@@ -50,7 +52,7 @@
      (capture scope-depth κ)
      #;(assign ℓ natural κ)]
   ;; give names to certain configurations for understandable metafunctions
-  [colorable ς-exit-tc ς-entry ς-call/cc]
+  [colorable ς-exit-tc ς-entry #;ς-call/cc]
   [ς-exit-tc ((clo (λ scope-depth () e) ρ) σ Ξ D (goto () () ρ scope-depth κ))
              (v σ Ξ D (goto () ((clo lam ρ) v ...) ρ scope-depth κ))]
   [ς-call (v σ Ξ D (push () ((clo lam ρ) v ...) ρ scope-depth κ))
@@ -59,25 +61,35 @@
   [ς-exit (v σ Ξ D (exit scope-depth κ))]
   [ς-final (v σ Ξ D halt)]
   [ς-intermediate-apply (v σ Ξ D (call (e_1 e ...) (v ...) ρ scope-depth κ))
-                        (v σ Ξ D (select d d scope-depth κ))]
-  [ς-call/cc ((clo (λ scope-depth (x) e) ρ) σ Ξ D (capture scope-depth κ))]
+                        (v σ Ξ D (call () (primop v ...) ρ scope-depth κ))
+                        (v σ Ξ D (select d d ρ scope-depth κ))]
+#;[ς-call/cc ((clo (λ scope-depth (x) e) ρ) σ Ξ D (capture scope-depth κ))]
   [ς-use/cc (v σ Ξ D (call () ((kont κ Ξ D)) ρ scope-depth κ))]
-  [push-kind push push-capture]
-  [exit-kind goto goto-capture])
+  [push-kind push #;push-capture]
+  [exit-kind goto #;goto-capture])
 (define-syntax-rule (is-state? pattern term)
   (not (not (redex-match Istack pattern term))))
-
 
 ;; These three meta-functions are the only ones needed to abstract
 #;(define-metafunction Istack ;; heap allocation
   [(alloc (v σ Ξ D κ) (natural ...))
    ,(add1 (max (max-nat-keys (term σ)) (max-nat-list (term (natural ...)))))])
 
-(define-metafunction Istack ;; heap allocation
-  [(alloc (v σ Ξ D κ) (natural ...)) 0])
+(define monovariance (make-parameter #f))
+(define var-count (make-parameter #f))
+(define (ref-bump x)
+  (dict-ref (monovariance) x (λ _ (define c (var-count)) (var-count (add1 c)) c)))
+
+(define-metafunction Istack
+  [(alloc ((clo (λ scope-depth (x ..._i) e) ρ) σ Ξ D (entry (v ..._i) scope-depth_unused κ))
+          (natural ...))
+   ,(ref-bump (list-ref (term (x ...)) (length (term (natural ...)))))]
+  [(alloc (v_n σ Ξ D (goto () ((clo (λ scope-depth (x ..._i x_n) e) ρ) v ..._i) ρ_unused scope-depth_unused κ))
+          (natural ...))
+   ,(ref-bump (list-ref (term (x ... x_n)) (length (term (natural ...)))))])
 
 #;(define-metafunction Istack ;; we want stack locations to be mutated
-  [(combine-stores σ_1 σ_2) ,(for/fold ([ret (term σ_1)]) ([(k vs) (in-dict (term σ_2))]) (dict-set ret k vs))])
+    [(combine-stores σ_1 σ_2) ,(for/fold ([ret (term σ_1)]) ([(k vs) (in-dict (term σ_2))]) (dict-set ret k vs))])
 (define-metafunction Istack ;; we want stack locations to be mutated
   [(combine-stores σ_1 σ_2)
    ,(for/fold ([ret (term σ_1)])
@@ -118,8 +130,8 @@
   [(display-lookup D scope-depth) ,(list-ref (term D) (term scope-depth))])
 (define-metafunction Istack
   [(ℓ-lookup σ ξ #t (Heap/Stack natural_h natural_s)) (,(list-ref (term ξ) (term natural_s)))]
-  [(ℓ-lookup σ ξ #f (Heap/Stack natural_h natural_s)) ,(dict-ref (term σ) (term natural_h))]
-  [(ℓ-lookup σ ξ #t (Stack natural)) (,(list-ref (term ξ) (term natural)))])
+  [(ℓ-lookup σ ξ #t (Stack natural_s))                (,(list-ref (term ξ) (term natural_s)))]
+  [(ℓ-lookup σ ξ #f (Heap/Stack natural_h natural_s)) ,(dict-ref (term σ) (term natural_h))])
 (define-metafunction Istack
   [(lookup ρ σ D (ann-ref x bool scope-depth))
    (ℓ-lookup σ (display-lookup D scope-depth) bool (env-lookup ρ x))])
@@ -167,7 +179,7 @@
    (where ξ (v ...))
    (where ((ℓ_all ...) σ_new) (color-alloc ς ((x v) ...)))]
   ;; call/cc
-  [(color (name ς ((clo (λ scope-depth (x) e) ρ) σ Ξ D (capture scope-depth_current κ))))
+#;[(color (name ς ((clo (λ scope-depth (x) e) ρ) σ Ξ D (capture scope-depth_current κ))))
    ((tail e)
     (env-extend ρ x ℓ)
     (combine-stores σ σ_new)
@@ -179,17 +191,17 @@
 
 (define-metafunction Istack
   [(color-alloc ς ((x v) ...))
-   ,(let* ([colors (map escapes? (term (x ...)))]
-           [heap-locs (box '())]
-           [σnew (box '())]
-           [escape->loc (λ (i b v) (cond [b (define hloc (term (alloc ς ,(unbox heap-locs))))
-                                          (set-box! heap-locs (cons hloc (unbox heap-locs)))
-                                          (set-box! σnew (term (combine-stores ,(unbox σnew) ((,hloc ,v)))))
-                                          (term (Heap/Stack ,hloc ,i))]
-                                       [else (term (Stack ,i))]))]
-           [locs (mapi escape->loc colors (term (v ...)))])
-      (printf "New heap ~a ~a~%~%" (redex-match Istack σ (unbox σnew)) (unbox σnew))
-      (term (,locs ,(unbox σnew))))])
+   ,(let ()
+      (define-values (heap-locs σnew) (values (box '()) (box '())))
+      (define (var->loc i x v)
+        (cond [(in-stack? x) (term (Stack ,i))]
+              [else (define hloc (term (alloc ς ,(unbox heap-locs))))
+                    (set-box! heap-locs (cons hloc (unbox heap-locs)))
+                    (set-box! σnew (term (combine-stores ,(unbox σnew) ((,hloc ,v)))))
+                    (term (Heap/Stack ,hloc ,i))]))
+      (define ℓs (mapi var->loc (term (x ...)) (term (v ...))))
+      (printf "Coloring: ~a ~a~%" ℓs (unbox σnew))
+      (term (,ℓs ,(unbox σnew))))])
 
 (define-metafunction Istack
   [(inject e) (e () () (()) (()) 0 halt)])
@@ -200,7 +212,8 @@
     ;; terminal eval states
     [--> (primop ρ σ Ξ D scope-depth κ) (primop σ Ξ D κ)]
     [--> (d ρ σ Ξ D scope-depth_current κ) (v σ Ξ D κ)
-         (where (v_pre ... v v_post ...) (lookup ρ σ D (untail d)))]
+         (where (name ref (ann-ref x bool scope-depth)) (untail d))
+         (where (v_pre ... v v_post ...) (lookup ρ σ D ref))]
     [--> (d ρ σ Ξ D scope-depth κ) (c σ Ξ D κ)
          (where (qwote c) (untail d))]
     [--> (d ρ σ Ξ D scope-depth κ) ((clo lam ρ) σ Ξ D κ)
@@ -210,10 +223,10 @@
     ;; intermediate eval states
     [--> ((e_0 e ...) ρ σ Ξ D scope-depth κ)             (e_0 ρ σ Ξ D scope-depth (push (e ...) () ρ scope-depth κ))]
     [--> ((if e_0 e_1 e_2) ρ σ Ξ D scope-depth κ)        (e_0 ρ σ Ξ D scope-depth (select e_1 e_2 ρ scope-depth κ))]
-    [--> ((call/cc e_0) ρ σ Ξ D scope-depth κ)           (e_0 ρ σ Ξ D scope-depth (push-capture scope-depth κ))]
+#;    [--> ((call/cc e_0) ρ σ Ξ D scope-depth κ)           (e_0 ρ σ Ξ D scope-depth (push-capture scope-depth κ))]
     [--> ((tail (e_0 e ...)) ρ σ Ξ D scope-depth κ)      (e_0 ρ σ Ξ D scope-depth (goto (e ...) () ρ scope-depth κ))]
     [--> ((tail (if e_0 e_1 e_2)) ρ σ Ξ D scope-depth κ) (e_0 ρ σ Ξ D scope-depth (select (tail e_1) (tail e_2) ρ scope-depth κ))]
-    [--> ((tail (call/cc e_0)) ρ σ Ξ D scope-depth κ)    (e_0 ρ σ Ξ D scope-depth (goto-capture scope-depth κ))]
+#;    [--> ((tail (call/cc e_0)) ρ σ Ξ D scope-depth κ)    (e_0 ρ σ Ξ D scope-depth (goto-capture scope-depth κ))]
     ;; apply states
     ;; intermediate evaluation
     [--> (v σ Ξ D (call (e_1 e ...) (v_0 ...) ρ scope-depth κ))
@@ -261,13 +274,15 @@
   (hash-set! Summary ς̃entry (set-add (hash-ref Summary ς̃entry (set)) ς̃exit)))
 
 (define (GCFA2 e)
-  (define e′ (escape-analysis! e coloring))
+  (monovariance (make-hasheq))
+  (var-count 0)
+  (define e′ (escape-analysis! e coloring #:annotate-static-depth? #t #:original-name-ht names))
   (define I (term (inject ,e′)))
   (define initial-work (list I I))
   (define (succ ς̃) (apply-reduction-relation R-Istack ς̃))
   (define (insert! k . hts) (for ([ht (in-list hts)]) (hash-set! ht k #t)))
   (define (Seen? pair) (hash-has-key? Seen pair))
-  (define name-ς values)
+  (define (name-ς ς) (strip-annotation ς names))
   ;; Summary : entry ↦ setof exit
   ;; Seen/Work : (list state state) ↦ #t
   ;; Callers/TCallers : entry ↦ (list caller entry)
@@ -276,9 +291,9 @@
   (define-values (Final Work) (values (make-hasheq) (make-hasheq)))
   (define (Update! ς̃₁ ς̃₂ ς̃₃ ς̃₄)
     (apply printf "Updating ~%〈~a,~% ~a,~% ~a,~% ~a〉~%" (map name-ς (list ς̃₁ ς̃₂ ς̃₃ ς̃₄)))
-    (term-let ([(v_1 σ_1 Ξ_1 D_1 (entry (v_i1 ...) scope-depth_1 truncated)) ς̃₁]
+    (term-let (#;[(v_1 σ_1 Ξ_1 D_1 (entry (v_i1 ...) scope-depth_1 truncated)) ς̃₁]
                [(v_n σ_2 Ξ_2 D_2 (push () (v_i2 ...) ρ_2 scope-depth_2 κ_2)) ς̃₂]
-               [(v_3 σ_3 Ξ_3 D_3 (entry (v_i3...) scope-depth_3 truncated)) ς̃₃]
+               [(v_3 σ_3 Ξ_3 D_3 (entry (v_i3 ...) scope-depth_3 truncated)) ς̃₃]
                [(v_4 σ_4 Ξ_4 D_4 (exit scope-depth_4 truncated)) ς̃₄])
       (Propagate! ς̃₁ (term (v_4 σ_4 Ξ_2 D_2 κ_2)))))
   (define (Propagate! ς̃₁ ς̃₂)
@@ -310,9 +325,11 @@
              [(is-state? ς-final ς̃₂) (printf "Final~%") (insert! ς̃₂ Final)]
 
              [(is-state? ς-exit ς̃₂) (printf "Return~%")
-              (add-summary! Summary (ς̃₁ ς̃₂))
-              (for-callers Callers (ς̃₃ ς̃₄ ς̃₁) (Update! ς̃₃ ς̃₄ ς̃₁ ς̃₂))
-              (for-callers TCallers (ς̃₃ ς̃₄ ς̃₁) (Propagate! ς̃₃ ς̃₂))]
+              (cond [(equal? ς̃₁ I) (insert! ς̃₂ Final)]
+                    [else
+                     (add-summary! Summary (ς̃₁ ς̃₂))
+                     (for-callers Callers (ς̃₃ ς̃₄ ς̃₁) (Update! ς̃₃ ς̃₄ ς̃₁ ς̃₂))
+                     (for-callers TCallers (ς̃₃ ς̃₄ ς̃₁) (Propagate! ς̃₃ ς̃₂))])]
 
              [(is-state? ς-exit-tc ς̃₂) (printf "Tail call~%")
               (for ([ς̃₃ (in-list (succ ς̃₂))])
@@ -320,8 +337,8 @@
                 (insert-caller! TCallers (ς̃₁ ς̃₂ ς̃₃))
                 (for-summary Summary (ς̃₃ ς̃₄) (Propagate! ς̃₁ ς̃₄)))]
 
-             [(is-state? ς-call/cc ς̃₂) (error 'todo-call/cc)]
-             [(is-state? ς-use/cc ς̃₂) (error 'todo-use/cc)]
+#;             [(is-state? ς-call/cc ς̃₂) (error 'todo-call/cc)]
+#;             [(is-state? ς-use/cc ς̃₂) (error 'todo-use/cc)]
 
              [else (error 'analyze "Uncaught case ~a~%" ς̃₂)])
        (analyze)])))
@@ -330,4 +347,6 @@
 (define esop* (translate-top esop))
 esop*
 (newline)
-;(GCFA2 esop*)
+(GCFA2 esop*)
+#;(define I (term (inject ,(escape-analysis! esop* coloring #:annotate-static-depth? #t #:original-name-ht names))))
+#;(apply-reduction-relation* R-Istack I)
